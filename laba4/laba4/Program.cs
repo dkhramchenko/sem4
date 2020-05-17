0813330f28ced0b36ee8b4e86580a0faade38e97
Qvocha <pineappletoe@gmail.com>
2020-05-17 23:59:54 +0300

    Update Program.cs

--- laba4/laba4/Program.cs
+++ laba4/laba4/Program.cs
@@ -11,7 +11,7 @@ namespace Laba4
     public class Node
     {
         // конструктор
-        public Node(char letter, int frequency)
+        public Node(char letter = 'x', int frequency = 0)
         {
             this.letter = letter;
             this.frequency = frequency;
@@ -88,6 +88,49 @@ namespace Laba4
             {
                 return 1;
             }
+
+            if (left != null && right == null)
+            {
+                return 1 + left.Height();
+            }
+
+            if (left == null && right != null)
+            {
+                return 1 + right.Height();
+            }
+
+            return Math.Max(left.Height(), right.Height()) + 1;
+        }
+
+        // метод возвращает список вершин уровня
+        public void Level(int i, ref List<Node> level)
+        {
+            if (this.level == i)
+            {
+                level.Add(this);
+                return;
+            }
+
+            if (left != null && right == null)
+            {
+                left.Level(i, ref level);
+                return;
+            }
+
+            if (left == null && right != null)
+            {
+                right.Level(i, ref level);
+                return;
+            }
+
+            if (left != null && right != null)
+            {
+                left.Level(i, ref level);
+                right.Level(i, ref level);
+                return;
+            }
+
+            return;
         }
 
         // поля
@@ -132,9 +175,76 @@ namespace Laba4
             return root.Height();
         }
 
+        public List<Node> Level(int i)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            List<Node> level = new List<Node>();
+            root.Level(i, ref level);
+            return level;
+        }
+
+
+        public void replaceLastNode()
+        {
+            int destinationLevel = 0;
+            for (int i = Height() - 1; i > 0; --i)
+            {
+                if (Level(i).Count < Convert.ToInt32(Math.Pow(2, i - 1)))
+                {
+                    destinationLevel = i;
+                }
+            }
+
+            List<Node> l = new List<Node>();
+            int k = 0;
+            l = Level(destinationLevel - 1);
+            for (int i = 0; i < l.Count; ++i)
+            {
+                if (l[i].left == null && l[i].right == null)
+                {
+                    k = i;
+                    break;
+                }
+            }
+
+            Node ln = l[k];
+            Node rn = ln.parent.right;
+
+            ln.left = new Node(ln.letter, ln.frequency);
+            ln.left.level = ln.level + 1;
+            ln.left.code = ln.code + "0";
+            ln.left.parent = ln;
+
+            Node R = Level(Height()).Last();
+            ln.right = new Node(R.parent.letter, R.parent.frequency);
+            ln.right.level = ln.level + 1;
+            ln.right.code = ln.code + "1";
+            ln.right.parent = ln;
+
+            rn.left = new Node(rn.letter, rn.frequency);
+            rn.left.code = rn.code + "0";
+            rn.left.parent = rn;
+            rn.left.level = rn.level + 1;
+
+            rn.right = new Node(R.parent.left.letter, R.parent.left.frequency);
+            rn.right.code = rn.code + "1";
+            rn.right.level = rn.level + 1;
+            rn.right.parent = rn;
+
+            R.parent.letter = R.letter;
+            R.parent.frequency = R.frequency;
+            Node temp = R.parent;
+            temp.left = null;
+            temp.right = null;
+        }
+
         public void decreaseCodes()
         {
-
+            
         }
 
         public Node root;
